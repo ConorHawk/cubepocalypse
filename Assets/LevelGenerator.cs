09dda4ec2d06fb2f18e15c747c46a68ec22fb272
ConorHawk <conornhawkins@gmail.com>
2017-11-24 14:55:31 +1300

    fixed procedural spawning

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -6,18 +6,21 @@ public class LevelGenerator : MonoBehaviour {
 
 	public GameObject standardBlock;
 	public GameObject tallBlock;
+	public GameObject level;
 
 	public int numberOfPlatforms = 200;
 	public float startOffset = 50f;
-	public float levelWidth = 1.5f;
 	public float randX = 1f;
 	public float frequency = 2f;
 	public float percentOfTall = 0.75f;
+
+	private float levelWidth;
 	
 	// Use this for initialization
 	void Start () {
 		Vector3 spawnPosition = new Vector3();
 		Vector3 tallSpawnPosition = new Vector3();
+		float levelWidth = level.transform.lossyScale.x;
 
 		for (int i = 0; i < numberOfPlatforms; i++)
 		{
@@ -25,21 +28,19 @@ public class LevelGenerator : MonoBehaviour {
 			float objSelector = Random.Range(0f,1f);
 			spawnPosition.z += Random.Range(interval-randX, interval+randX)*10+startOffset;
 			tallSpawnPosition.z += Random.Range(interval-randX, interval+randX)*10+startOffset;
-			tallSpawnPosition.x = Random.Range(-levelWidth, levelWidth);
+			tallSpawnPosition.x = Random.Range(-7, 7);
 
 			if (objSelector > percentOfTall){
-				Instantiate(tallBlock, tallSpawnPosition, Quaternion.identity);
+				spawnBlock(tallSpawnPosition, tallBlock);
 			} 
-			Instantiate(tallBlock, tallSpawnPosition, Quaternion.identity);
-			Instantiate(tallBlock, tallSpawnPosition, Quaternion.identity);
+			// Instantiate(tallBlock, tallSpawnPosition, Quaternion.identity);
+			// Instantiate(tallBlock, tallSpawnPosition, Quaternion.identity);
 			
-			spawnBlock(interval, standardBlock);
-			
+			spawnBlock(spawnPosition, standardBlock);
 		}
 	}
 
-	void spawnBlock (float interval, GameObject objectToSpawn){
-
+	void spawnBlock (Vector3 spawnPosition, GameObject objectToSpawn){
 		Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
 	}
 	
