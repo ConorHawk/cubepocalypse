d5311b183a239ec2d6ce96ee799fff2bd50fd3ad
ConorHawk <conornhawkins@gmail.com>
2017-11-28 14:47:28 +1300

    added infinite level

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,6 +2,7 @@
 
 public class PlayerCollision : MonoBehaviour {
 
+    public GameObject level;
     public playerMovement movement;
     public float spawnDistance = 50f;
     public float spawnMin = 0.8f;
@@ -26,5 +27,6 @@ public class PlayerCollision : MonoBehaviour {
         Vector3 PlayerLocation = new Vector3(0,0,transform.position.z);
         float spawnVariation = spawnDistance * Random.Range(spawnMin, spawnMax);
         triggerObj.gameObject.transform.parent.gameObject.transform.position = PlayerLocation + Vector3.forward * spawnVariation;
+        level.transform.position = PlayerLocation + Vector3.back * 2;
     }
 }
---
+++ Assets/highscore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highscore : MonoBehaviour {
+	public Text highscoreUi;
+    public Transform playerPos;
+
+	public static int highscoreNumber = 0;
+
+	void Start()
+    {
+        // scoreUi.text = 0.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+		if(highscoreNumber < Mathf.RoundToInt(playerPos.transform.position.z)){
+			highscoreNumber = Mathf.RoundToInt(playerPos.transform.position.z);
+		}
+        highscoreUi.text = highscoreNumber.ToString();
+    }
+}
